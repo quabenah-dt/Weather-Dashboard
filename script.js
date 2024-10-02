495adf4b3d17921e0d407e5085788d6725c0bf6d
Dortey Tetteh <emmanueltdortey@yahoo.com>
2024-10-02 14:51:36 +0000

    "Added not found page, updated script.js to handle API calls and error handling, and modified styles in style.css"

--- script.js
+++ script.js
@@ -27,21 +27,85 @@ searchInput.addEventListener('blur', function() {
     }
 });
 
+// container
+const container = document.querySelector('.container');
 
+// API KEY
+const apiKey = '9d751db450eb388f855509b8ae7bccb8';
+
+// NOT FOUND
+const notFound = document.getElementById('not-found-container');
+
+// weather info
+const weatherInfo = document.querySelector('.weatherinfo');
+
+// daily report
+const dailyReport = document.querySelector('.daily-report');
+
+// weather icon
+const weatherIcon = document.querySelector('.weather-icon img');    
+
+// temperature
+const temperature = document.querySelector('.temp');
+
+// humidity
+const humidity = document.querySelector('.humidity');
+
+// wind speed
+const windSpeed = document.querySelector('.wind-speed');
+
+// wind direction
+const windDirection = document.querySelector('.wind-direction');
+
+// pressure
+const pressure = document.querySelector('.pressure');
+
+// visibility
+const visibility = document.querySelector('.visibility');   
 
 // USER LOCATION
+const userLocation = document.getElementById('user-location');
 
-const userLocation = document.getElementById('user-location')
-
-userLocation.addEventListener('change', function() {
+userLocation.addEventListener('change', async function() {
     if (userLocation.value.trim() !== '') {
-        updateWeatherInfo();
+        await updateWeatherInfo(userLocation.value);
         userLocation.value = '';
         userLocation.blur();
     }
 });
 
-function updateWeatherInfo(userLocation) {
-    const weatherData = getFetchData();
+async function getFetchData(endpoint, userLocation) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endpoint}?q=${userLocation}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
 }
+
+async function updateWeatherInfo(userLocation) {
+    const weatherData = await getFetchData('weather', userLocation);
+   
+    if (weatherData.cod !== 200) {
+        showDisplayError(notFound);
+        return;
+    }
+
+    showWeatherInfo(weatherData);
+} 
+
+function showDisplayError(notFound) {
+    notFound.style.display = 'block';
+    container.style.display = 'none';
+}
+
+function showWeatherInfo(weatherData) {
+    notFound.style.display = 'none';
+    container.style.display = 'block';
+}
+
 
